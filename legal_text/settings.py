8c7dce13060797a7e437c38c131bd18250b2ff69
devsmarak972 <smarakkanjilal@gmail.com>
2024-08-19 05:03:37 +0530

    functionality integrated
    updated commit

--- legal_text/settings.py
+++ legal_text/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-GROQ_API_KEY = 'api_key'
+GROQ_API_KEY = '<api_key>'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
